4db20ab3d3d567c7ef95149378d8dfb0dc3f7748
JoaoTS20 <joaots@ua.pt>
2021-10-24 23:31:04 +0100

    Add usage of command line arguments to fcm

--- Group_name/src/fcm.py
+++ Group_name/src/fcm.py
@@ -1,8 +1,7 @@
 import re
 import math
 import random
-import cProfile
-
+import sys
 
 class FCM:
     
@@ -18,7 +17,7 @@ class FCM:
     #Count all subsequences of k+1 words
     def count_subsequences(self,words,k):
         dic = {}
-        for i in range(0,len(words)):
+        for i in range(0,len(words),k+1):
             seq = tuple(words[i:i+(k+1)])
             if len(seq) < k+1:
                 continue  
@@ -29,30 +28,27 @@ class FCM:
         return dic
 
     #Get probability of a state c in the text
-    def frequency_sequence(self,seq,total_len_seq):
+    def frequency_sequence(self,text,seq,k):
         t_seq = tuple(seq)
-
         if t_seq in self.fs_cache:
-            count = self.fs_cache[t_seq]
-            result = count/total_len_seq
-            return result
-        return 0
+            return self.fs_cache[t_seq]
+        total_seq = [text[i:i+k] for i in range(0,len(text),k+1)]
+        remain_seq = [i for i in total_seq if i == seq ]
+
+        result = len(remain_seq)/len(total_seq)
+        self.fs_cache[t_seq] = result
+        return result
     
     #Calculate all probabilities for each context
     def calculate_probabilities(self):
-        words_len = len(self.words)
         unique_words = set(self.words)
         seq_count = self.count_subsequences(self.words,self.k)
-
         # Sequences Dictionary
         # for key in seq_count:
         #     print(f'{key} -> {seq_count[key]}')
         prob_dic = {}
         contexts_seen = {}
-
-
-
-        for i in range(0,words_len):
+        for i in range(0,len(self.words),self.k+1):
             c = self.words[i:i+self.k]
             if tuple(c) in contexts_seen:
                 continue
@@ -60,13 +56,12 @@ class FCM:
             sum_all_constexts_c = sum([ seq_count[tuple(c + [simbol])] for simbol in unique_words if tuple(c + [simbol]) in seq_count])
             all_probs = []
             #Get all possible sequences for c + appending with another simbol
-            unique_words_len = len(unique_words)
             for simbol in unique_words:
                 seq = tuple(c + [simbol])
                 if(seq not in seq_count):
                     continue
                 n = seq_count[seq]
-                total = (sum_all_constexts_c + self.a * unique_words_len)
+                total = (sum_all_constexts_c + self.a * len(unique_words))
                 #Prob of event e for context c
                 prob_n = (n + self.a)/total
                 #print(f'Seq:{seq} Nprob:{prob_n} the total from prob is {total}')
@@ -80,36 +75,17 @@ class FCM:
         self.prob_dic = prob_dic
         return prob_dic
 
-    def calculate_frequencies(self,total_seq):
-        for i in total_seq:
-            seq_hash = tuple(i)
-            if seq_hash in self.contexts_seen:
-                if seq_hash in self.fs_cache:
-                    self.fs_cache[seq_hash]+=1
-                else:
-                    self.fs_cache[seq_hash] = 0
-
     #Calculate entropy taking into account 
     def calculate_entropy(self):
-        words_len = len(self.words)
         context_entropy_list = []
         if self.contexts_seen == None: return 0
-        
-        #Total number of sequences
-        total_seq = [self.words[i:i+self.k] for i in range(0,words_len)]
-        total_seq_len = len(total_seq)
-        
-        #Calculate frequencies before hand
-        #Done in order to prevent multiple transversals of the whole text
-        self.calculate_frequencies(total_seq)
-
         for c in self.contexts_seen:
             all_probs = self.contexts_seen[c]
             #Get Hc entropy for each context/row
             entropy_of_context = -1 * sum([prob * math.log(prob,2) for prob in all_probs])
             #Probability of context/subsequence in total text
-            prob_c = self.frequency_sequence(list(c),total_seq_len)
-            #print(f'For c={c} entropy={entropy_of_context} Cprob={prob_c}')
+            prob_c = self.frequency_sequence(self.words,list(c),self.k)
+            # print(f'For c={c} entropy={entropy_of_context} Cprob={prob_c}')
             context_entropy_list.append(entropy_of_context * prob_c)
         return sum(context_entropy_list)
         
@@ -121,29 +97,38 @@ def read_text(address):
         return text_letters
 
 
-
-#TODO:Check number of CPU's/cores available for PC,
-#If 1 or None do all in one process, else divide
-#list in N sublists and give each one to a process
-#Might have to wait out others
-
-
 def main():
-
+    args = sys.argv[1:]
+    if len(args) == 6 and args[0] == '-a' and args[2] == '-k' and args[4] == '-textpath':
+        a= int(args[1])
+        k=int(args[3])
+        textpath=args[5]
+        text = read_text(textpath)
+        fcm = FCM(text,a,k)
+        prob_dic = fcm.calculate_probabilities()
+        entropy = fcm.calculate_entropy()
+        for key in prob_dic:
+            print(f'{key} : {prob_dic[key]}')
+        print(f'Smoothing: {a} and Order: {k}')
+        print(f'Entropy:{entropy}')
+    else:
+        print("Error: Bad use of Command Line Argument!")
+        print("Usage:$ python3 fcm.py -a <smoothing_parameter> -k <order_of_the_model> -textpatch <path_of_the_text_file> ")
+        sys.exit()
+    """
     a = 0
 
-    k = 3
+    k = 2
         
-    text = read_text('../example/example.txt')
+    text = read_text('../example/example2.txt')
 
     fcm = FCM(text,a,k)
     prob_dic = fcm.calculate_probabilities()
     entropy = fcm.calculate_entropy()
-    # for key in prob_dic:
-    #     print(f'{key} : {prob_dic[key]}')
-    #print(f'Smoothing: {a} and Order: {k}')
-    #print(f'Entropy:{entropy}')
+    for key in prob_dic:
+        print(f'{key} : {prob_dic[key]}')
+    print(f'Smoothing: {a} and Order: {k}')
+    print(f'Entropy:{entropy}')"""
 
 if __name__ == "__main__":
-    #cProfile.run('main()',sort='tottime')
     main()
